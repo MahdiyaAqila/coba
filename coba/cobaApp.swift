81bc87b12886789777cf0f4f3298f653c3f15032
MahdiyaAqila <66235613+MahdiyaAqila@users.noreply.github.com>
2023-06-21 09:18:02 +0700

    Initial Commit

---
+++ coba/cobaApp.swift
@@ -0,0 +1,17 @@
+//
+//  cobaApp.swift
+//  coba
+//
+//  Created by Mahdiya Aqila on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct cobaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
